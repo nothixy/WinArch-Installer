e9122bdad2b243d1e8a5a6b5db9795897ae651bd
srgoti <fl.valentin1904@gmail.com>
2020-11-11 19:59:30 +0100

    I hate my ideas

---
+++ Slideshow.xaml.cs
@@ -0,0 +1,88 @@
+﻿/*    WinArch installer - a Windows executable to install Archlinux on your PC
+    Copyright (C) 2020  srgoti
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.*/
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Net;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Path = System.IO.Path;
+
+namespace WinArch
+{
+    /// <summary>
+    /// Interaction logic for Slideshow.xaml
+    /// </summary>
+    public partial class Slideshow : Page
+    {
+        public Slideshow()
+        {
+            InitializeComponent();
+            downloadLatestGrub();
+        }
+        public static async Task<string> downloadLatestGrub()
+        {
+
+            // Get the object used to communicate with the server.
+            FtpWebRequest request = (FtpWebRequest)WebRequest.Create("ftp://ftp.gnu.org/gnu/grub/");
+            request.Method = WebRequestMethods.Ftp.ListDirectoryDetails;
+
+            // This example assumes the FTP site uses anonymous logon.
+            request.Credentials = new NetworkCredential("anonymous", "");
+
+            FtpWebResponse response = (FtpWebResponse)request.GetResponse();
+
+            Stream responseStream = response.GetResponseStream();
+            StreamReader reader = new StreamReader(responseStream);
+            string[] files = reader.ReadToEnd().Split('\n');
+            reader.Close();
+            response.Close();
+            List<string> filenames = new List<string>();
+            for (int i = 0; i < (files.Length - 1); i++)
+            {
+                if (Regex.IsMatch(Regex.Replace(Regex.Replace(files[i], ".*\\sgrub-", "grub-"), "\\s*", ""), "grub-[0-9\\.]*-for-windows.zip(?!\\.sig)"))
+                {
+                    filenames.Add(Regex.Replace(Regex.Replace(files[i], ".*\\sgrub-", "grub-"), "\\s*", ""));
+                }
+            }
+            string filenamemaster = filenames[0];
+            if (filenames.Count > 1)
+            {
+                for (int i = 1; i < filenames.Count; i++)
+                {
+                    if (String.Compare(filenamemaster, filenames[i]).ToString() == "-1")
+                    {
+                        filenamemaster = filenames[i];
+                    }
+                }
+            }
+            WebClient client = new WebClient();
+            client.Credentials = new NetworkCredential("anonymous", "");
+            client.DownloadFile("ftp://ftp.gnu.org/gnu/grub/" + filenamemaster, Path.GetTempPath() + "grub.zip");
+        }
+    }
+}
--- User.xaml.cs
+++ User.xaml.cs
@@ -1,4 +1,19 @@
-﻿using System;
+﻿/*    WinArch installer - a Windows executable to install Archlinux on your PC
+    Copyright (C) 2020  srgoti
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.*/
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Diagnostics;
@@ -58,11 +73,7 @@ namespace WinArch
             {
                 Application.Current.Properties["Uname"] = unameBox.Text;
             }
-            Application.Current.Properties["Uname"] = unameBox.Text;
-            foreach(DictionaryEntry de in Application.Current.Properties)
-            {
-                Debug.WriteLine(de.Key + ": " + de.Value.ToString());
-            }
+            Application.Current.Properties["UnameSys"] = unameBox.Text;
             NavigationService nav = this.NavigationService;
             nav.Navigate(new Uri("Desktop.xaml", UriKind.Relative));
         }
